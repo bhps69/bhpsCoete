7beaa664e405aec64f8fe4623600cf3289d3d76e
phani bh <phanibhandaru@gmail.com>
2019-02-13 17:14:35 +0530

    data table

--- wp-content/themes/bridge-child/TrainEval1.php
+++ wp-content/themes/bridge-child/TrainEval1.php
@@ -23,21 +23,17 @@ add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');?>
 <head>
 <script>
 	function clickMe(e){
-		alert(e);
 		var frm = document.getElementById('cityForm');
 		frm.submit();
-			
-	
+	    
 	}
-		function clickMe1(e){
-		alert(e);
+		function clickMe1(e1){
 		var frm1 = document.getElementById('stateForm');
 		frm1.submit();
 		
 	}
 
-		function clickMe2(e){
-		alert(e);
+		function clickMe2(e2){
 		var frm2 = document.getElementById('countryForm');
 		frm2.submit();
 		
@@ -45,10 +41,11 @@ add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');?>
 
 </script>
 </head>
-<body id="body" >
+<body id="body">
 
 <!-- data-table -->
 <div class="content" >
+
 <div class="container-fluid tab-info-sec" >
     <div class="row">	
     <div class="roaster-table col-md-12 col-md-offset-2">
@@ -66,22 +63,28 @@ add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');?>
 		  <th>Name</th>
 		  <th>id</th>
 		  <th>email</th>
-		  <th><form action="#" method="POST" name="cityForm" id="cityForm"><select name="city" id='city_name' style="{border: 0px;outline:0px;-webkit-border-radius:0px}"><option selected="selected" value="">City</option><option value="florida">Florida</option><option value="Mount Vernon">Mount Vernon</option><option value="Seattle">Seattle</option><option value="East Rutherford">East Rutherford</option></select> <input type="Submit" id="submitMe" class="submitMe"  onclick="clickMe(this.id);"/></form></th>
+		  <th><form action="#" method="POST" name="cityForm" id="cityForm"><select name="city" id='city_name' style="{border: 0px;outline:0px;-webkit-border-radius:0px}"  onchange="clickMe(this.value)"><option selected="selected" value="%">City</option><option value="All">All</option><option value="Hyderabad">Hyderabad</option><option value="Florida">Florida</option><option value="Mount Vernon">Mount Vernon</option><option value="Seattle">Seattle</option><option value="East Rutherford">East Rutherford</option></select> </form></th>
 		  
-		  <th><form action="#" method="post" name="stateForm" id="stateForm"><select name="city" style="{border: 0px;outline:0px;-webkit-border-radius:0px}"><option selected="selected">State</option><option value="florida">IOWA</option><option value="Mount Vernon">washington</option><option value="Seattle">Alabama</option><option value="East Rutherford">Idaho</option></select>
-		  <input type="Submit" id="submitMe1" class="submitMe1"  onclick="clickMe1(this.id);"/>
+		  <th><form action="#" method="POST" name="stateForm" id="stateForm"><select name="state" style="{border: 0px;outline:0px;-webkit-border-radius:0px}" onchange="clickMe1(this.value)"><option value="%" selected="selected">State</option><option value="All">All</option><option value="IOWA">iowa</option><option value="washington">washington</option><option value="Alabama">alabama</option><option value="Idaho">Idaho</option><option value='arizona'>arizona</option><option value='conecticut'>connecticut</option></select></form>
+		  
 		  </th>
 		  
-          <th><form action="#" method="post" name="countryForm"><select name="city" style="{border: 0px;outline:0px;-webkit-border-radius:0px}"><option selected="selected" value="">Country</option><option value="florida">usa</option><option value="Mount Vernon">India</option><option value="Seattle">bangladesh</option><option value="East Rutherford">china</option></select>
-		  <input type="Submit" id="submitMe2" class="submitMe2"  onclick="clickMe2(this.id);"/>
+          <th><form action="#" method="POST" name="countryForm" id="countryForm"><select name="country" style="{border: 0px;outline:0px;-webkit-border-radius:0px}" onchange="clickMe2(this.value)"><option selected="selected" value="%">Country</option><option value="All">All</option><option value="usa">usa</option><option value="India">India</option><option value="bangladesh">bangladesh</option><option value="china">china</option></select>
+		  </form>
 		  </th>
 		  
 		</tr>
 	</thead>
 	<tbody>
             <?php 
-			function getData(){
-                $args = array(
+			
+				
+			function getData($filter){
+				
+				$users='';
+				if($filter==''){
+				
+				 $args = array(
                     'role__in' => ['evaluator', 'trainer'],
                     'meta_query' => array(
                         array(
@@ -89,37 +92,71 @@ add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');?>
                             'value'   => get_user_meta($current_user->ID,'mepr_company_name',true),
                             'compare' => 'LIKE'
                         )
-                    )
-                );
-				$users = get_users( $args );					
+                    ));
+				}
+				else
+				{
+
+
+					$args	= array(
+                    'role__in' => ['evaluator', 'trainer'],
+                    'meta_query' => array(
+                        $filter,
+						array(
+                            'key'     => 'mepr_company_name',
+                            'value'   => get_user_meta($current_user->ID,'mepr_company_name',true),
+                            'compare' => 'LIKE'
+							)
+						)
+					);
+				}
+
+
+					$users = get_users( $args );		
+
 				return $users;
 			}
-                
-				$users=getData();
+			
+			if(isset($_POST['city']) and $_POST['city']<>'All'){
+				echo 'inside city if';
+				$cityFilter = array('key'=>'mepr_city', 'value'=>$_POST['city'], 'compare'=>'LIKE');
+				$users = getData($cityFilter);
+
+			}
+			elseif(isset($_POST['state']) and $_POST['state']<>'All'){
+
+				$stateFilter = array('key'=>'mepr_state_province', 'value'=>$_POST['state'], 'compare'=>'LIKE');
+				$filter=$stateFilter;
+				$users = getData($filter);
 				
-				print_r($_POST);
+			}
+			elseif(isset($_POST['country']) and $_POST['country']<>'All'){
+				$countryFilter = array('key'=>'mepr_country', 'value'=>$_POST['country'], 'compare'=>'LIKE');
+
+				$filter=$countryFilter;
+				$users = getData($filter);
+			}
+			elseif($_POST['city']=='All'||$_POST['state']=='All'||$_POST['country']='All')
+				$users = getData(''); 
+			
+				
+			
+				
 				
                 if (!empty($users)) {					
-//           
+
 					foreach($users as $key=>$user) {
 					$city = get_user_meta($user->ID,'mepr_city',true);					
-/*					if($_POST[0]<>"" and $city<>$_POST[0])
-						continue;
-					else*/
-						$selCity = $city;
-  					$state = get_user_meta($user->ID,'mepr_state_province',true);  
-						$selState=$state;
- 					$country = get_user_meta($user->ID,'mepr_country',true);
-					$selCountry=$country;					?>
-					
+					$state= get_user_meta($user->ID,'mepr_state_province',true);
+					$country = get_user_meta($user->ID,'mepr_country',true);?>
                 <tr>
-                    <td ><?php echo $user->user_login; ?></td>
-                    <td width="50px"><?php echo $user->display_name; ?></td>
-                    <td width="100%"><?php echo $user->ID?></td>
+                    <td><?php echo $user->user_login;?></td>
+                    <td width="50px"><?php echo $user->display_name;?></td>
+                    <td width="100%"><?php echo $user->ID;?></td>
                     <td width="100%"><?php echo $user->user_email;?></td>
-					<td><?php echo $selCity;?></td>
-					<td><?php echo $selState;?></td>
-					<td><?php echo $selCountry;?></td>        
+					<td><?php echo $city;?></td>
+					<td><?php echo $state;?></td>
+					<td><?php echo $country;?></td>        
                 </tr>
                 <?php 
                     
@@ -131,9 +168,7 @@ add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');?>
 </table>
 </div>
 <div class="table-btn">
-    <a href="<?php echo site_url(); ?>/send-invite?type=evaluator">Add Evaluator</a>
-    <a href="javascript:;" class="deactiveAcc">Save Change</a>
-</div>
+   </div>
 </div>
 <div class="clearfix"></div>
 </div>
